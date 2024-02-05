41796c93a614ebef224d01f11c5976c3cf9cbb65
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2024-02-04 21:25:55 -0300

    adjust service

--- src/services/configApi.js
+++ src/services/configApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3022/', // Substitua pela URL base da sua API
+    baseURL: 'http://api.modelojuaprofessor.com.br/', // Substitua pela URL base da sua API
     headers: {
         'Content-Type': 'application/json',
         'authorization': '123456'
